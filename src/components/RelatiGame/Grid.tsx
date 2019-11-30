32642491103149efa86c4b503c427809d11806fb
fixiabis <fixiabis@gmail.com>
2019-11-30 12:48:10 +0800

    更新說明與移除不必要的變數

--- src/components/RelatiGame/Grid.tsx
+++ src/components/RelatiGame/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 import { RelatiGrid, RelatiSymbolColor } from "../../modules/game";
 
 interface GridProps { grid: RelatiGrid; focus?: boolean; }
