67430e369b48d7e534be8e2d0f57f3a9e8761839
fixiabis <fixiabis@gmail.com>
2019-11-30 12:35:40 +0800

    更新說明頁面

--- src/views/HelpPage/HelpPage.tsx
+++ src/views/HelpPage/HelpPage.tsx
@@ -21,6 +21,15 @@ class HelpPage extends React.Component<AppProps> {
 
     return (
       <div id="help-page" className="page">
+        <div className="description">
+          <h2>遊戲玩法</h2>
+          <p>遊戲開始時，可選擇棋盤上任何空格下子</p>
+          <p>遊戲開始後，只能在棋子連線範圍內下子</p>
+          <p>跨格連線中間經過的格子必須為空格，倘若之後對方下子在空格時，該連線將會失效</p>
+          <p>當原本的連線失效時，棋子將會尋找新的連線方式，若找不到時，該棋子的連線範圍將會失效</p>
+          <p>當連線範圍失效的棋子找到新的連線方式時，該棋子的連線範圍將會恢復</p>
+          <p>當對方無法繼續下子時，即為我方的勝利</p>
+        </div>
         <div className="button-group">
           <button className="exit" onClick={this.leavePage} />
         </div>
